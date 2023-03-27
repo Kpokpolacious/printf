9ae87f42a7cfcc3e2eba8cf50769416c17872500
Kpokpolacious <nedolisaraphael@gmail.com>
2023-03-27 13:58:28 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,53 +1,64 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
  * @format: format.
- * Return: Printed chars.
+ * Return: count.
  */
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	va_list args;
+	va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	int count = 0;
+	for (int a = 0; format[a] != '\0'; a++)
 	{
-		if (format[i] != '%')
+		if (format[a] == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			a++;
+			if (format[a] == 'd')
+			{
+				int value = va_arg(args, int);
+				count += printf("%d", value);
+			}
+			else if (format[a] == 'f')
+			{
+				double value = va_arg(args, double);
+				count += printf("%f", value);
+			}
+			else if (format[a] == 's')
+			{
+				char *value = va_arg(args, char *);
+				count += printf("%s", value);
+			}
+			else if (format[i] == 'c')
+			{
+				char value == va_arg(args, int);
+				count += putchar("%c", value);
+			{
+			else
+			{
+				count += putchar('%');
+				count += putchar(format[i]);
+			}
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			count += putchar(format[i]);
 		}
 	}
+	va_end(args);
+	return (count);
+}
 
-	print_buffer(buffer, &buff_ind);
+int main(void)
+{
+	_printf("Hello, world!\n");
+	_printf("My name is %s, I am %d years old\n", "Raph-r7", 1);
+	_printf("The value of %s is %f\n", "PI", 3.14124);
 
-	va_end(list);
-
-	return (printed_chars);
+	return (0);
 }
