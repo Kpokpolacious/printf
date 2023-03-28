9b13bd412c63037e8785d18c6d713c4feaf30fff
Kpokpolacious <nedolisaraphael@gmail.com>
2023-03-28 16:14:11 -0700

    task 1 passed

--- handle_printf.c
+++ handle_printf.c
@@ -15,6 +15,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
+
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
